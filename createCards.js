f7f25de6f2d9135de650823fc5b491b65aac307c
Salma Ismaili <131111@helenparkhurst.net>
2015-12-15 23:14:18 +0100

    randomDeck afgemaakt, kan getest worden. Verder wat aangepast in createCards.

--- createCards.js
+++ createCards.js
@@ -14,22 +14,22 @@
 function createQuark(particle, anti, colour){
     switch(particle){
         case 'u': 
-            return {particle: "u", generation: 1, anti: anti, colour: colour, mass: 5, charge: 0.667};
+            return {particle: "u", generation: 1, anti: anti, colour: colour, mass: 5, charge: 0.667, order: 0};
             break;
         case 'd': 
-            return {particle: "d", generation: 1, anti: anti, colour: colour, mass: 10, charge: -0.333};
+            return {particle: "d", generation: 1, anti: anti, colour: colour, mass: 10, charge: -0.333, order: 0};
             break;
         case 'c':
-            return {particle: "c", generation: 2, anti: anti, colour: colour, mass: 1, charge: 0.667};
+            return {particle: "c", generation: 2, anti: anti, colour: colour, mass: 1, charge: 0.667, order: 0};
             break;
         case 's':
-            return {particle: "s", generation: 2, anti: anti, colour: colour, mass: 200, charge: -0.333};
+            return {particle: "s", generation: 2, anti: anti, colour: colour, mass: 200, charge: -0.333, order: 0};
             break; 
         case 't':
-            return {particle: "t", generation: 3, anti: anti, colour: colour, mass: 175, charge: 0.667};
+            return {particle: "t", generation: 3, anti: anti, colour: colour, mass: 175, charge: 0.667, order: 0};
             break;
         case 'b':
-            return {particle: "b", generation: 3, anti: anti, colour: colour, mass: 4, charge: -0.333};
+            return {particle: "b", generation: 3, anti: anti, colour: colour, mass: 4, charge: -0.333, order: 0};
             break;
         default: 
     }
