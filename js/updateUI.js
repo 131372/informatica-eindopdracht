6481cf6251f233817bc62b61931a398397034354
Mick Gielen <131372@hpleerling.nl>
2016-01-19 09:04:35 +0100

    Completed task "Updaten van de turnorder"

--- js/updateUI.js
+++ js/updateUI.js
@@ -5,3 +5,19 @@ function updateHand(){
 function updateCombination(){
 
 }
+
+function updateTurnOrder(){
+	$html="Beurten<br>";
+	first=true;
+	for(i=gameObject['currentPlayer'];i<=gameObject['playerAmount'];i++){
+		//if(first){
+		//	$html+="Aan het spelen:";
+		//	first=false;
+		//}
+		$html+=gameObject['players'][i]+", "+gameObject['points'][i]+"pt, "+Object.keys(gameObject['hands'][i]).length+"kaarten<br>";
+	}
+	for(i=1;i<gameObject['currentPlayer'];i++){
+		$html+=gameObject['players'][i]+", "+gameObject['points'][i]+"pt, "+Object.keys(gameObject['hands'][i]).length+"kaarten<br>";
+	}
+	$("#TurnOrder").html($html);
+}
