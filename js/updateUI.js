7839f7f23d7e2a88aa7f49424ed30b4dd2dc1d32
Ryan Schoop <132072@helenparkhurst.net>
2016-01-23 16:55:22 +0100

    Made some modifications 'Updaten combinatie van de andere spelers' and 'Updaten combinatie van de huidige speler'  should also be completed now.

--- js/updateUI.js
+++ js/updateUI.js
@@ -1,18 +1,33 @@
-function updateUIAppendCards(cards, element, message, htmlclass, idPrefix){ // latter three are optional
+function updateUIAppendCards(cards, combination, element, height, width, message, htmlclass, idPrefix){ // latter three are optional, if combination read cards as combinations
     htmlclass = (typeof htmlclass === 'undefined') ? "" : htmlclass; 
     message = (typeof message === 'undefined') ? "" : message; // not sure if I should control message here for certain cases.
-    idPrefix = (typeof idPrefix === 'undefined') ? "" : idPrefix;
+    idPrefix = (typeof idPrefix === 'undefined') ? $(element).not('#') : idPrefix; // should write a test for this
+    height = height + ""; // should write a test for this.
+    width = width + "";
     var path = "images/";
     $(element).empty();
     $(element).append("<p>" + message + "</p></br>");
     for(var i = 0; i < cards.length; i++){
-        var cardGraphic = cardGraphic(cards[i]);
-        $(element).append("<img id='" + idPrefix + cardGraphic['graphic'] +"' " + "class='" + htmlclass +  "' " + "src='" + path + cardGraphic[0] + "' " + ">");
+        if(combination){
+            var cardGraphic = cardGraphic(cards[i][0]); 
+            cardGraphic["id"] = ""; // recognize the combination by id.
+            for(var j = 0; j < cards[i].length; j++){
+                if(j == 0){
+                    cardGraphic["id"] += cards[i][0]['particle'] + cards[i][0]['name'] + cards[i][0]['anti'] + cards[i][0]['colour']; // cards represented in the same way as the pictures.
+                } else {
+                    cardGraphic["id"] += "," + cards[i][0]['particle'] + cards[i][0]['name'] + cards[i][0]['anti'] + cards[i][0]['colour']; // comma seperate the entities.
+                }
+            }
+        } else {
+            var cardGraphic = cardGraphic(cards[i]);
+        }
+        $(element).append("<img id='" + idPrefix + cardGraphic['graphic'] +"' " + 
+                "class='" + htmlclass +  "' " + 
+                "src='" + path + cardGraphic[0] + "' " + 
+                "height='" + height + "' " + 
+                "width='" + width + "' " + 
+                ">");
     } 
-}
-
-function updateCombination(){
-
 }
 
 function updateTurnOrder(){
