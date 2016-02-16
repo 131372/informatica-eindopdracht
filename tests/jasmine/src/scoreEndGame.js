da93f660bac7b27360c498a94a04d60059cad2f4
unknown <119682@WKS-HP-1739.ASG.LOCAL>
2016-02-16 09:28:38 +0100

    Test

--- tests/jasmine/src/scoreEndGame.js
+++
@@ -1,9 +0,0 @@
-function scoreEndGame(deck, hands, currentPlayerCombinations){
-    if (checkForGameEnd(deck, hands)){
-        gameObject["points"][gameObject.currentplayer] += 3;  
-    }
-   
-    if (checkForProtonNeutronGameEnd(currentPlayerCombinations)){
-        gameObject["points"][gameObject.currentplayer] += 3; 
-    }
-}
